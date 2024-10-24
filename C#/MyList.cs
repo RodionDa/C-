52d41c7c4aa9403633f0392e463c708607efd430
Rodion <r.dausht@xlab-game.com>
2024-10-24 20:23:53 +0300

    Home work

--- C#/MyList.cs
+++ C#/MyList.cs
@@ -1,15 +1,18 @@
-public class MyList
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class MyList<T> : IEnumerable<T>
 {
-    private int[] _array = new int[4]; // Хранит элементы списка
+    private T[] _array = new T[4]; // Хранит элементы списка
     private int _count = 0; // Счетчик добавленных элементов
 
-    public int Count 
-    {
-        get { return _count; } // Возвращает текущее количество элементов
-    }
+    public int Count => _count; // Возвращает текущее количество элементов
+    public int Capacity => _array.Length; // Возвращает емкость списка
 
     // Добавляет элемент в список
-    public void Add(int item) 
+    public void Add(T item) 
     {
         if (_count == _array.Length) 
         {
@@ -19,11 +22,11 @@ public class MyList
     }
 
     // Удаляет первый найденный элемент из списка
-    public void Remove(int item) 
+    public void Remove(T item) 
     {
         for (int i = 0; i < _count; i++) 
         {
-            if (_array[i] == item) 
+            if (EqualityComparer<T>.Default.Equals(_array[i], item)) 
             {
                 for (int j = i; j < _count - 1; j++) 
                 {
@@ -48,7 +51,7 @@ public class MyList
     }
 
     // Вставляет элемент по индексу
-    public void Insert(int index, int item) 
+    public void Insert(int index, T item) 
     {
         if (index < 0 || index > _count) throw new IndexOutOfRangeException();
         
@@ -70,6 +73,47 @@ public class MyList
         _count = 0; // Сбрасываем счетчик
     }
 
+    // Выполняет указанное действие для каждого элемента
+    public void ForEach(Action<T> action)
+    {
+        for (int i = 0; i < _count; i++)
+        {
+            action(_array[i]); // Выполняем действие для элемента
+        }
+    }
+
+    // Находит индекс первого вхождения указанного элемента
+    public int IndexOf(T item)
+    {
+        for (int i = 0; i < _count; i++)
+        {
+            if (EqualityComparer<T>.Default.Equals(_array[i], item))
+            {
+                return i; // Возвращаем индекс, если найдено
+            }
+        }
+        return -1; // Возвращаем -1, если не найдено
+    }
+
+    // Находит первый элемент, который соответствует заданному предикату
+    public T Find(Func<T, bool> predicate)
+    {
+        for (int i = 0; i < _count; i++)
+        {
+            if (predicate(_array[i]))
+            {
+                return _array[i]; // Возвращаем элемент, если найдено
+            }
+        }
+        return default; // Возвращаем значение по умолчанию, если не найдено
+    }
+
+    // Сортирует элементы в списке
+    public void Sort(Comparison<T> comparison)
+    {
+        Array.Sort(_array, 0, _count, Comparer<T>.Create(comparison)); // Сортируем массив
+    }
+
     // Возвращает строковое представление списка
     public override string ToString() 
     { 
@@ -80,8 +124,19 @@ public class MyList
     private void Resize() 
     {
         int newSize = _array.Length * 2; // Увеличиваем размер вдвое
-        int[] newArray = new int[newSize];
+        T[] newArray = new T[newSize];
         Array.Copy(_array, newArray, _count); // Копируем старые элементы
         _array = newArray; // Заменяем старый массив новым
     }
+
+    // Реализация IEnumerable<T>
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (int i = 0; i < _count; i++)
+        {
+            yield return _array[i]; // Возвращаем элементы для перебора
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator(); // Реализация интерфейса IEnumerable
 }
